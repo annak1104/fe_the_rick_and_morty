43086923bf0f93859b2932b2e97fa39d093fdf98
Andrii Nakonechnyi <andreynakonechnuy11@mail.com>
2023-12-06 21:59:52 +0200

    feat: create homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import { Header } from './components/Header/Header';
 import { Banner } from './components/Banner/Banner';
 import { Footer } from './components/Footer/Footer';
-import { CardsList } from './components/CardsList/CardsList';
-import { Filter } from './components/Filter/Filter';
-import { PaginationComponent } from './components/PaginationComponent/PaginationComponent';
+import { HomePage } from './pages/HomePage/HomePage';
 
 export const App = () => {
   return (
     <>
     <Header />
     <Banner />
-    <Filter />
-    <CardsList />
-    <PaginationComponent />
+    <HomePage />
     <Footer />
     </>
   );
